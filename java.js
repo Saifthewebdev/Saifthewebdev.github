eb9e4472a0f14f8e27930fce7fc13350ddc6961f
Saifthewebdev <152909743+Saifthewebdev@users.noreply.github.com>
2023-12-04 20:59:58 +0300

    Added files via drag and drop

---
+++ java.js
@@ -0,0 +1,7 @@
+// You can replace these with your actual company name and description
+const companyName = "Your Car Maintenance Company";
+const companyDescription = "We specialize in providing high-quality car maintenance services. Our team of skilled technicians ensures that your vehicle is in top-notch condition.";
+
+// Update company name and description
+document.getElementById("companyName").innerText = companyName;
+document.getElementById("companyDescription").innerHTML = `<p>${companyDescription}</p>`;
